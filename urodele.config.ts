c6a3c12588d927f01578f5204468f5eb47e00fb4
4u <mtvboy@gmail.com>
2026-05-01 17:04:09 +0800

    Update urodele.config.ts

--- urodele.config.ts
+++ urodele.config.ts
@@ -12,7 +12,7 @@ export const config = {
   },
   footer: {
     copyright: "© 北京智享出行科技发展有限公司",
-    copyrightUrl: "https://blog.zxcx.qzz.io",
+    copyrightUrl: "blog.zxcx.qzz.io",
   },
   pagination: {
     size: 10,
